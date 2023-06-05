81ea96c358bfccda617dcf794f9ee7420e6321f5
Lorenzo Bertolini <ciao@lorenzobertolini.com>
2023-06-05 16:40:42 +0200

    setup printable document 1350x1350 pixels

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Previewer } from "pagedjs";
+import { useLayoutEffect } from "react";
+import { range } from "lodash";
+
+function MyLogo({ dark = false }: { dark?: boolean }) {
+  const color = dark ? "text-white" : "text-[#222]";
+  return (
+    <div
+      className={`LogoToRemoveFromFlow ${color} font-inter font-light text-3xl tracking-tight`}
+    >
+      lorenzo<span className="font-semibold">bertolini</span>
+    </div>
+  );
+}
 
 function App() {
+  useLayoutEffect(() => {
+    (document.querySelector("#preview") as Element).innerHTML = "";
+
+    const previewer = new Previewer();
+    previewer
+      .preview(
+        (document.querySelector("#root") as Element).innerHTML,
+        ["pagedjs-styles.css"],
+        document.querySelector("#preview")
+      )
+      .then((flow: any) => {
+        console.log("preview rendered, total pages", flow.total, { flow });
+      });
+    return () => {
+      document.head
+        .querySelectorAll("[data-pagedjs-inserted-styles]")
+        .forEach((e) => e.parentNode?.removeChild(e));
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* logo will be removed from document flow */}
+      <MyLogo />
+      {/*  */}
+      <div style={{ breakBefore: "page" }}>
+        <div className="flex justify-center items-center h-[1200px] bg-orange-500 rounded-sm overflow-hidden">
+          <div className="flex flex-col gap-8">
+            <div className="text-6xl font-inter font-semibold text-center leading-snug">
+              The Web Data Visualization Series
+            </div>
+            <div className="text-9xl font-inter font-semibold text-center leading-snug">
+              {"<circle />"}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*  */}
+      <div style={{ breakBefore: "page" }}>
+        <div className="flex justify-center items-center h-[1200px] bg-orange-500 rounded-sm overflow-hidden">
+          <svg width={1200} height={1200} className="bg-orange-500">
+            <circle cx={600} cy={600} r={100} fill="yellow" />
+          </svg>
+        </div>
+      </div>
+      {/*  */}
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "caveat-brush": ['"Caveat Brush"', "cursive"],
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = "") {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey];
+
+          const newVars =
+            typeof value === "string"
+              ? { [`--color${colorGroup}-${colorKey}`]: value }
+              : extractColorVars(value, `-${colorKey}`);
+
+          return { ...vars, ...newVars };
+        }, {});
+      }
+
+      addBase({
+        ":root": extractColorVars(theme("colors")),
+      });
+    },
+  ],
+};
